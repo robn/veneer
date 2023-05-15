dccc41fe3d8a7c59e14c9e3569b8303f97d12111
Rob Norris <robn@despairlabs.com>
2023-05-15 21:30:10 +1000

    start of lifting common command setup

--- src/main.rs
+++ src/main.rs
@@ -5,51 +5,46 @@ use std::error::Error;
 use std::ffi::{CStr,CString};
 use std::fs::File;
 
-fn ioc_pool_configs(z: &mut File) -> Result<nvlist::Header, Box<dyn Error>> {
+fn zc_new() -> (ioctls::Command, Box<[u8; 262144]>) {
     let mut zc: ioctls::Command = Default::default();
-    let buf: [u8; 262144] = [0; 262144];
+    let buf: Box<[u8; 262144]> = Box::new([0; 262144]);
     zc.nvlist_dst = buf.as_ptr();
     zc.nvlist_dst_size = 262144;
+    (zc, buf)
+}
+
+fn ioc_pool_configs(z: &mut File) -> Result<nvlist::Header, Box<dyn Error>> {
+    let (mut zc, mut buf) = zc_new();
     ioctls::ZFS_IOC_POOL_CONFIGS.ioctl(z, &mut zc).unwrap();
     let nvbuf = &buf[0..zc.nvlist_dst_size as usize];
     Ok(nvlist::unpack(nvbuf)?)
 }
 
 fn ioc_pool_stats(z: &mut File, pool: &CStr) -> Result<nvlist::Header, Box<dyn Error>> {
-    let mut zc: ioctls::Command = Default::default();
+    let (mut zc, mut buf) = zc_new();
     let name = pool.to_bytes_with_nul();
     zc.name[..name.len()].copy_from_slice(&name);
-    let buf: [u8; 262144] = [0; 262144];
-    zc.nvlist_dst = buf.as_ptr();
-    zc.nvlist_dst_size = 262144;
     ioctls::ZFS_IOC_POOL_STATS.ioctl(z, &mut zc);
     let nvbuf = &buf[0..zc.nvlist_dst_size as usize];
     Ok(nvlist::unpack(nvbuf)?)
 }
 
 fn ioc_objset_stats(z: &mut File, objset: &CStr) -> Result<nvlist::Header, Box<dyn Error>> {
-    let mut zc: ioctls::Command = Default::default();
+    let (mut zc, mut buf) = zc_new();
     let name = objset.to_bytes_with_nul();
     zc.name[..name.len()].copy_from_slice(&name);
-    let buf: [u8; 262144] = [0; 262144];
-    zc.nvlist_dst = buf.as_ptr();
-    zc.nvlist_dst_size = 262144;
     ioctls::ZFS_IOC_OBJSET_STATS.ioctl(z, &mut zc)?;
     let nvbuf = &buf[0..zc.nvlist_dst_size as usize];
     Ok(nvlist::unpack(nvbuf)?)
 }
 
 fn ioc_dataset_list_next(z: &mut File, dataset: &CStr, cookie: u64) -> Result<(CString, nvlist::Header, u64), Box<dyn Error>> {
-    let mut zc: ioctls::Command = Default::default();
+    let (mut zc, mut buf) = zc_new();
     let name = dataset.to_bytes_with_nul();
     zc.name[..name.len()].copy_from_slice(&name);
     zc.cookie = cookie;
-    let buf: [u8; 262144] = [0; 262144];
-    zc.nvlist_dst = buf.as_ptr();
-    zc.nvlist_dst_size = 262144;
     ioctls::ZFS_IOC_DATASET_LIST_NEXT.ioctl(z, &mut zc)?;
     let nvbuf = &buf[0..zc.nvlist_dst_size as usize];
-    //println!("{:#?}", zc);
     Ok((CStr::from_bytes_until_nul(&zc.name)?.into(), nvlist::unpack(nvbuf)?, zc.cookie))
 }
 
