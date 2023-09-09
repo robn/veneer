51876d28b20c25ffc49245244a1282a563efdced
Rob Norris <robn@despairlabs.com>
2023-09-09 11:29:21 +1000

    AutoString: more conversions for use as a dict key

--- src/util.rs
+++ src/util.rs
@@ -5,12 +5,40 @@
 // Copyright (c) 2023, Rob Norris <robn@despairlabs.com>
 
 use std::cell::OnceCell;
+use std::cmp::Ordering;
 use std::ffi::{CStr, CString};
 use std::fmt;
 
 // this badly-named thing takes a CString and does a lossy conversion to String on demand, which is
 // what you want almost always
+#[derive(Clone)]
 pub(crate) struct AutoString(CString, OnceCell<String>);
+
+impl PartialEq for AutoString {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.eq(&other.0)
+    }
+}
+
+impl Eq for AutoString {}
+
+impl Ord for AutoString {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl PartialOrd for AutoString {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Default for AutoString {
+    fn default() -> Self {
+        AutoString(CString::new([]).unwrap(), OnceCell::new())
+    }
+}
 
 impl fmt::Display for AutoString {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -37,5 +65,11 @@ impl From<&CStr> for AutoString {
 impl From<AutoString> for String {
     fn from(s: AutoString) -> Self {
         s.to_string()
+    }
+}
+
+impl From<AutoString> for CString {
+    fn from(s: AutoString) -> Self {
+        s.0.clone()
     }
 }
