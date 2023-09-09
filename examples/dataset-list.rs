0c2e5c43a4a0e1d99149f4d2b3cc162f302606f4
Rob Norris <robn@despairlabs.com>
2023-09-09 20:11:39 +1000

    Dataset type and setup
    Phew, what a ride this was. Doing more than we strictly need to for just
    this call, and no doubt things are gonna get kinda crazy later once I
    try to start using the property cache but at least its here.

--- examples/dataset-list.rs
+++ examples/dataset-list.rs
@@ -4,32 +4,15 @@
 
 // Copyright (c) 2023, Rob Norris <robn@despairlabs.com>
 
-// XXX convert to high-level api, when we have one
-
 use std::error::Error;
-use std::ffi::CString;
-use std::io::Error as IOError;
-use veneer::ioc;
+use veneer::zfs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut ioc = ioc::Handle::open()?;
+    let z = zfs::open()?;
 
-    let configs = ioc.pool_configs()?;
-    for pool in configs.keys() {
-        let mut stack: Vec<(CString, u64)> = vec![(pool.into(), 0)];
-        while let Some((name, cookie)) = stack.pop() {
-            match ioc.dataset_list_next(&name, cookie) {
-                Ok(is) => {
-                    stack.push((name, is.cookie));
-                    let name = is.name.into();
-                    println!("{:?}", name);
-                    stack.push((name, 0));
-                }
-                Err(e) => {
-                    let ioe = e.downcast::<IOError>()?;
-                    ioe.raw_os_error().filter(|n| *n == 3).ok_or(ioe)?; // ESRCH
-                }
-            }
+    for pool in z.pools()? {
+        for dataset in pool.datasets()? {
+            println!("{}", dataset.name());
         }
     }
 
--- src/zfs.rs
+++ src/zfs.rs
@@ -7,13 +7,17 @@
 use crate::ioc;
 use crate::nvpair::PairList;
 use crate::util::AutoString;
+use elsa::FrozenMap;
 use std::cell::{OnceCell, RefCell};
 use std::error::Error;
+use std::ffi::{CStr, CString};
+use std::io::Error as IOError;
 use std::rc::Rc;
 
 struct Handle {
     ioc: RefCell<ioc::Handle>,
     config: OnceCell<PairList>,
+    datasets: FrozenMap<CString, Box<PairList>>,
 }
 
 impl Handle {
@@ -21,6 +25,7 @@ impl Handle {
         Ok(Handle {
             ioc: RefCell::new(ioc::Handle::open()?),
             config: OnceCell::default(),
+            datasets: FrozenMap::default(),
         })
     }
 
@@ -35,6 +40,50 @@ impl Handle {
         let _ = self.config.set(c);
 
         Ok(self.config.get().unwrap())
+    }
+
+    fn get_dataset(&self, name: &CStr) -> Result<&PairList, Box<dyn Error>> {
+        if let Some(p) = self.datasets.get(name) {
+            return Ok(p);
+        }
+
+        let p = self.ioc.borrow_mut().objset_stats(name)?;
+        self.datasets.insert(name.into(), Box::new(p));
+
+        Ok(self.datasets.get(name).unwrap())
+    }
+
+    fn get_dataset_list(&self) -> Result<Vec<&CStr>, Box<dyn Error>> {
+        let mut list: Vec<&CStr> = vec![];
+
+        for pool in self.get_config()?.keys() {
+            let _ = self.get_dataset(pool)?;
+            list.push(pool);
+
+            let mut stack: Vec<(CString, u64)> = vec![(pool.into(), 0)];
+            while let Some((name, cookie)) = stack.pop() {
+                match self.ioc.borrow_mut().dataset_list_next(&name, cookie) {
+                    Ok(is) => {
+                        stack.push((name, is.cookie));
+                        let name = is.name.clone();
+                        list.push(match self.datasets.get_key_value(&name) {
+                            Some((k, _)) => k,
+                            None => {
+                                self.datasets.insert(name.clone(), Box::new(is.list));
+                                self.datasets.get_key_value(&name).unwrap().0
+                            }
+                        });
+                        stack.push((name, 0));
+                    }
+                    Err(e) => {
+                        let ioe = e.downcast::<IOError>()?;
+                        ioe.raw_os_error().filter(|n| *n == 3).ok_or(ioe)?; // ESRCH
+                    }
+                }
+            }
+        }
+
+        Ok(list)
     }
 }
 
@@ -67,6 +116,35 @@ pub struct Pool {
 impl Pool {
     fn new(handle: Rc<Handle>, name: AutoString) -> Pool {
         Pool { handle, name }
+    }
+
+    pub fn name(&self) -> String {
+        self.name.to_string()
+    }
+
+    pub fn datasets(&self) -> Result<Vec<Dataset>, Box<dyn Error>> {
+        Ok(self
+            .handle
+            .get_dataset_list()?
+            .iter()
+            .filter(|ds| {
+                ds.to_bytes().starts_with(self.name.as_bytes())
+                    && (ds.to_bytes().len() == self.name.as_bytes().len()
+                        || ds.to_bytes()[self.name.as_bytes().len()] == b'/')
+            })
+            .map(|ds| Dataset::new(self.handle.clone(), ds.into()))
+            .collect())
+    }
+}
+
+pub struct Dataset {
+    handle: Rc<Handle>,
+    name: AutoString,
+}
+
+impl Dataset {
+    fn new(handle: Rc<Handle>, name: AutoString) -> Dataset {
+        Dataset { handle, name }
     }
 
     pub fn name(&self) -> String {
