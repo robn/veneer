0faf86377926484968cc451c1e13226ab1d0ede2
Rob Norris <robn@despairlabs.com>
2023-09-23 15:09:17 +1000

    Extend and tableise pool-list
    Still not there, just playing with some concepts.

--- examples/pool-list.rs
+++ examples/pool-list.rs
@@ -5,21 +5,40 @@
 // Copyright (c) 2023, Rob Norris <robn@despairlabs.com>
 
 use std::error::Error;
-use veneer::zfs;
+use tabled::{builder::Builder, settings::Style};
+use veneer::zfs::{self, Vdev};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let z = zfs::open()?;
 
+    let mut tb = Builder::default();
+    tb.set_header(["name", "state", "read", "write", "cksum", "slow"]);
+
+    let mut push_vdev = |name: String, vd: &Vdev| -> Result<(), Box<dyn Error>> {
+        let vs = vd.stats()?;
+        tb.push_record([
+            format!("{}", name),
+            format!("{}", vs.state),
+            format!("{}", vs.read_errors),
+            format!("{}", vs.write_errors),
+            format!("{}", vs.checksum_errors),
+            format!("{}", vs.slow_ios),
+        ]);
+        Ok(())
+    };
+
     for pool in z.pools()? {
-        println!("{}", pool.name());
-
         let root = pool.root_vdev()?;
-        println!("  {}", root.guid());
 
-        for c in root.children()? {
-            println!("    {}", c.guid());
+        push_vdev(pool.name(), &root)?;
+
+        for vd in root.children()? {
+            push_vdev(vd.guid().to_string(), &vd)?;
         }
     }
+
+    let table = tb.build().with(Style::rounded()).to_string();
+    println!("{}", table);
 
     Ok(())
 }
