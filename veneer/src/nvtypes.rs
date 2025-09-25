c8eb11fd410e0142706d34bb1e609441eb4c37b7
Rob Norris <robn@despairlabs.com>
2025-09-25 16:30:24 +1000

    all: copyright date bump

--- veneer/src/nvtypes.rs
+++ veneer/src/nvtypes.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Copyright (c) 2023, Rob Norris <robn@despairlabs.com>
+// Copyright (c) 2023-2025, Rob Norris <robn@despairlabs.com>
 
 // XXX this and other structures like it in fs/zfs.h can be extended with
 //     new versions, but not reduced. so we need to initialise to zero, and
