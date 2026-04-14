7cb7c450828e792c6038dd75674fadeef4ec0391
Rob Norris <robn@despairlabs.com>
2026-04-14 16:05:31 +1000

    dash: license headers

--- dash/src/data.rs
+++ dash/src/data.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Copyright (c) 2023-2026, Rob Norris <robn@despairlabs.com>
+
 use veneer::VdevState;
 
 #[derive(Clone, Debug, Default)]
@@ -10,4 +16,3 @@ pub(crate) struct PoolData {
     pub write: u64,
     pub _wat: String,
 }
-
